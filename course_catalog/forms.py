76be65d89f6788e40ffe05bbea4232be073c5ed7
Chris <chris@cssko.me>
2016-01-03 19:10:22 -0500

    Added initial forms, views, and urls to course_catalog.

--- course_catalog/forms.py
+++ course_catalog/forms.py
@@ -1,1 +1,64 @@
-# TODO Add forms
+from django import forms
+from haystack.forms import FacetedSearchForm
+
+from course_catalog.models import Term
+
+
+BY_CHOICES = [('id', 'Course ID'), ('disc', 'Discipline'), ('title', 'Course Title'),
+              ('desc', 'Course Description'), ('all', 'all')]
+ORDER_CHOICES = [('id', 'Course ID'), ('disc', 'discipline')]
+GRAD_CHOICES = [('both', 'Both'), ('undergrad', 'Undergraduate'), ('grad', 'Graduate')]
+
+
+class CourseSearchForm(FacetedSearchForm):
+    q = forms.CharField(required=False)
+    i = forms.ChoiceField(required=False, choices=BY_CHOICES)
+    sort = forms.ChoiceField(required=False, choices=ORDER_CHOICES)
+    grad = forms.ChoiceField(required=False, choices=GRAD_CHOICES)
+
+    def search(self):
+        sqs = super(CourseSearchForm, self).search()
+
+        if not self.is_valid():
+            return self.no_query_found()
+        q = self.cleaned_data['q']
+        i = self.cleaned_data['i']
+        sort = self.cleaned_data['sort']
+        grad = self.cleaned_data['grad']
+
+        if i != '':
+            if i == 'id':
+                sqs = sqs.filter(course_id__startswith=q)
+            # if i == 'disc':
+            #     sqs = sqs.filter(discipline_exact=string.upper(q))
+            if i == 'title':
+                sqs = sqs.filter(course_title=q)
+            if i == 'desc':
+                sqs = sqs.filter(course_description=q)
+            if i == 'all':
+                pass
+
+        if sort != '':
+            if sort == 'id':
+                sqs = sqs.order_by('id')
+            if sort == 'disc':
+                sqs = sqs.order_by('discipline')
+
+        if grad != '':
+            if grad == 'both':
+                pass
+            if grad == 'undergrad':
+                sqs = sqs.filter(graduate_level='undergraduate')
+            if grad == 'grad':
+                sqs = sqs.filter(graduate_level='graduate')
+
+        return sqs.facet('discipline')
+
+
+class CourseTermForm(forms.Form):
+    term = forms.ChoiceField(required=False,
+                             choices=tuple((i.term_code, i.term_name) for i in (Term.objects.all())))
+
+    def get_term(self):
+        term = self.cleaned_data['term']
+        return term
--- course_catalog/urls.py
+++ course_catalog/urls.py
@@ -1,1 +1,11 @@
-#  TODO Add urls.
+from django.conf.urls import url
+
+from course_catalog.views import catalog_index_view, discipline_table, discipline_courses_view, CourseSectionsView
+
+app_name = 'course_catalog'
+urlpatterns = [
+    url(r'^$', view=catalog_index_view, name='catalog'),
+    url(r'^discipline_table/', view=discipline_table, name='discipline_table'),
+    url(r'^discipline/(?P<discipline>[A-Z]{2,4})/$', view=discipline_courses_view, name='discipline'),
+    url(r'^course/(?P<course>[A-Z]{2,4}[0-9]{4}[A-Z]?)/$', view=CourseSectionsView.as_view(), name='course')
+]
