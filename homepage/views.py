cf9d7753523169a34b4e3429f0e685d5b038b0cb
cssko <kothmacr@mail.uc.edu>
2018-02-20 18:04:48 -0500

    Starting over from Django 2.0 and moving to a postgresql backend

--- homepage/views.py
+++ homepage/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render
 
-
-def index(request):
-    return render(request, 'index.html')
-
-
-def contact_page(request):
-    return render(request, 'contact.html')
-
-
-def about_page(request):
-    return render(request, 'about.html')
+# Create your views here.
