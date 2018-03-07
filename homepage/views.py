e131667a25c2f40001fac7cefa0ca2362c23e17f
cssko <kothmacr@mail.uc.edu>
2018-03-07 11:58:36 -0500

    Returning a PDF resume and partially implementing a contact form

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponseRedirect, FileResponse, Http404
 from django.shortcuts import render
 from django.views import View
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 
 class IndexView(View):
@@ -7,7 +10,6 @@ class IndexView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {'active_nav': 'index'})
-
 
 
 class WorkExperience(View):
@@ -25,14 +27,25 @@ class Activities(View):
 
 
 class Resume(View):
-    template_name = 'home/resume.html'
+    # template_name = 'home/resume.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'active_nav': 'resume'})
+        try:  # Let's just return the pdf for now
+            return FileResponse(open('media/Chris Kothman.pdf', 'rb'), content_type='application/pdf')
+        except FileNotFoundError:
+            raise Http404()
+        # return render(request, self.template_name, {'active_nav': 'resume'})
 
 
 class Contact(View):
     template_name = 'home/contact_me.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'active_nav': 'contact'})
+        form = ContactForm()
+        return render(request, self.template_name, {'active_nav': 'contact', 'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            pass
+        return render(request, self.template_name, {'form': form})
